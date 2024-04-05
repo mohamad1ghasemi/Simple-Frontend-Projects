afb4c6dc81543c459dd92fc79a7517400529b217
mohamad <78101279+mohamad1ghasemi@users.noreply.github.com>
2024-04-05 15:24:13 +0330

    update first project

---
+++ beginner/helloWorld.js
@@ -0,0 +1,14 @@
+/* create simple project Hello World*/
+
+// 1. Using console.log()
+
+console.log("hello world")
+
+// 2. Using alert
+
+alert("hello world!!")
+
+// 3.Using document.write()
+document.write('hello world')
+// 4. Using innerHTML 
+document.getElementById('demo').innerHTML = "hello world";
