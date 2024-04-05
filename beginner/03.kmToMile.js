bed7f0ad24ec1e9339712ec5706901d253165768
mohamad <78101279+mohamad1ghasemi@users.noreply.github.com>
2024-04-05 16:17:28 +0330

    create 04.shuffleOfCard.js

---
+++ beginner/03.kmToMile.js
@@ -0,0 +1,7 @@
+// We know that 1 kilometer equal to 0.621371 miles. So:
+
+const kilometers = prompt("enter Your value: ");
+const factor = 0.621371;
+const miles = kilometers * factor;
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`)
