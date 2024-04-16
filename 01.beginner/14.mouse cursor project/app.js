c2b3be00ff8eb3df85150399759f603773d47e17
mohamad <78101279+mohamad1ghasemi@users.noreply.github.com>
2024-04-16 11:27:23 +0330

    update 15 to 18 beginner project

--- 01.beginner/14.mouse cursor project/app.js
+++ 01.beginner/14.mouse cursor project/app.js
@@ -1,9 +1,6 @@
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", function (event) {
-  // event.pageX --> across
-  // event.pageY --> up and down
-  // console.log(event);
   moveCursor(event.pageX, event.pageY);
 });
 
